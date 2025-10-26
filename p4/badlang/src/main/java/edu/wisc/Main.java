df90334e782df9f2b1d55428d6ff6de4ae31f396
Tyler Richard <tylerrichard215@gmail.com>
2025-10-26 12:05:29 -0500

    Add P4

---
+++ p4/badlang/src/main/java/edu/wisc/Main.java
@@ -0,0 +1,20 @@
+package edu.wisc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Main entry point for the badlang semantic analyzer.
+ * 
+ * This program performs name analysis and type checking on badlang source files.
+ * 
+ * Usage: java edu.wisc.Main <source-file>
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+    }
+       
+}
---
+++ p4/badlang/src/main/java/edu/wisc/VarType.java
@@ -0,0 +1,8 @@
+package edu.wisc;
+
+public enum VarType {
+    INT,
+    BOOL
+}
+
+
